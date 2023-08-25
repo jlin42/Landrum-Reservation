71d38a4f4dd4670ed28bfd82317bf496011b9df6
jlin42 <jlin14@wm.edu>
2023-08-25 10:31:57 -0400

    Make timeslot child of Calendar

--- src/pages/Bookings.js
+++ src/pages/Bookings.js
@@ -1,13 +1,11 @@
 import ReactCalendar from "../components/Calendar"; 
 import 'react-calendar/dist/Calendar.css'
-import TimeSlot from '../components/Timeslots';
 // import './App.css';
 
 function Booking() {
 return (
  <div>
     <ReactCalendar/>
-    <TimeSlot/>
   </div>
   );
 }
