15f6996844eef23fbce163e1986473f68f5c57d2
jlin42 <jlin14@wm.edu>
2023-09-05 23:53:43 -0400

    Automatically update timeslots at midnight est

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,7 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import TimeSlot from './Timeslots';
 import './css/Calendar.css';
-import { getFirestore, collection, getDocs, where, query, setDoc, doc } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, where, query, setDoc, doc, deleteDoc } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '../config/firebase-config';
 
@@ -33,41 +33,77 @@ const ReactCalendar = () => {
   
     for (const date of dates) {
       const formattedDate = date.toISOString().split('T')[0];
-  
-      try {
-        const docRef = doc(collectionRef, formattedDate);
-        const docData = {
-            time: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM"],
-            availability: true
-            
+      const currentDate = new Date();
+      const currentHour = currentDate.getHours();
+
+      if (currentHour === 0){
+            try {
+                const docRef = doc(collectionRef, formattedDate);
+                const docData = {
+                    time: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM"],
+                    availability: true
+                    
+                };
+        
+                // Create the document
+                await setDoc(docRef, docData);
+        
+                console.log(`Document created for ${formattedDate}`);
+            } catch (error) {
+                console.error(`Error creating document for ${formattedDate}:`, error);
+            }
+            }
         };
-  
-        // Create the document
-        await setDoc(docRef, docData);
-  
-        console.log(`Document created for ${formattedDate}`);
-      } catch (error) {
-        console.error(`Error creating document for ${formattedDate}:`, error);
       }
-    }
-  };
+
+    const deleteOldBookings = async () => {
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0];
+    
+        // Calculate midnight of the current day
+        const midnight = new Date(today);
+        midnight.setHours(0, 0, 0, 0);
+    
+        const collectionRef = collection(db, 'bookings');
+        const q = query(collectionRef, where('date', '<', formattedDate));
+    
+        try {
+        const querySnapshot = await getDocs(q);
+    
+        querySnapshot.forEach(async (doc) => {
+            // Check if the booking is older than midnight
+            if (doc.data().date.toMillis() < midnight.getTime()) {
+            // Delete the old booking
+            await deleteDoc(doc.ref);
+            console.log(`Deleted old booking: ${doc.id}`);
+            }
+        });
+        } catch (error) {
+        console.error('Error deleting old bookings:', error);
+        }
+      };
+      
+      // Schedule the function to run every midnight
+      setInterval(deleteOldBookings, 24 * 60 * 60 * 1000);
+  
+      
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const formattedDate = selectedDate.toISOString().split('T')[0];
-        const timeslotsRef = collection(db, 'bookings');
-        const q = query(timeslotsRef, where('date', '==', formattedDate));
-        const querySnapshot = await getDocs(q);
+            const formattedDate = selectedDate.toISOString().split('T')[0];
+            const timeslotsRef = collection(db, 'bookings');
+            const q = query(timeslotsRef, where('date', '==', formattedDate));
+            const querySnapshot = await getDocs(q);
 
-        if (querySnapshot.size > 0) {
-          const data = querySnapshot.docs[0].data();
-          setTimeslots(data.timeslots || []);
-        } else {
-          setTimeslots([]);
-        }
-      } catch (error) {
-        console.error('Error fetching timeslots: ', error);
+            if (querySnapshot.size > 0) {
+            const data = querySnapshot.docs[0].data();
+            setTimeslots(data.timeslots || []);
+            } else {
+            setTimeslots([]);
+            }
+        } catch (error) {
+            console.error('Error fetching timeslots: ', error);
       }
     };
 
